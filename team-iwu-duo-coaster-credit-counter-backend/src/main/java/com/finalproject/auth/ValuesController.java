601d13b65bccee4512caa82f276be39654bd74c2
dharding03 <73205955+dharding03@users.noreply.github.com>
2021-06-14 22:25:43 -0400

    Add files via upload

---
+++ team-iwu-duo-coaster-credit-counter-backend/src/main/java/com/finalproject/auth/ValuesController.java
@@ -0,0 +1,12 @@
+package com.finalproject.auth;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/values")
+
+
+public class ValuesController {
+
+}
---
+++ team-iwu-duo-coaster-credit-counter-backend/src/test/java/com/danharding/finalproject/TeamIwuDuoCoasterCreditCounterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danharding.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamIwuDuoCoasterCreditCounterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
